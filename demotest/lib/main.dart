6b9f9fa66ef56152c2686e8a0edc984118d365b5
unknown <mahmud22205101398>
2024-09-05 15:37:30 +0600

    sending demo 1

---
+++ demotest/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 80, 96, 104),
+        title: const Center(
+          child: Text(
+            "I am rich.",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      body: const Center(
+          child: Image(
+              image: NetworkImage(
+                  "https://static.vecteezy.com/system/resources/previews/008/513/899/original/blue-diamond-illustration-png.png"))),
+    ),
+  ));
+}
